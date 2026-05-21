74c1f2c998a3a38dab43def5809dbe59a56a34b9
The Dumb <info@hemisphera.com>
2026-05-21 10:39:46 +0200

    Use OSC as transport

---
+++ Hsp.LedStripController/GmemToOscDispatcher.cs
@@ -0,0 +1,59 @@
+﻿using Hsp.Osc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ReaSharp;
+
+namespace Hsp.LedStripController;
+
+public class GmemToOscDispatcher : BackgroundService
+{
+  private readonly GmemService _memService;
+  private readonly IOscClient _oscClient;
+  private readonly ILogger<GmemToOscDispatcher> _logger;
+  private const int NumLedStrips = 4;
+  private const int NumSegmentsPerLedStrips = 12;
+
+
+  public GmemToOscDispatcher(GmemService memService, IOscClient oscClient, ILogger<GmemToOscDispatcher> logger)
+  {
+    _memService = memService;
+    _oscClient = oscClient;
+    _logger = logger;
+  }
+
+
+  public override async Task StartAsync(CancellationToken cancellationToken)
+  {
+    await base.StartAsync(cancellationToken);
+    _memService.Connect("ledcontroller");
+    await _oscClient.ConnectAsync();
+    _logger.LogInformation("OSC dispatcher started.");
+  }
+
+  protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+  {
+    var block = new double[NumSegmentsPerLedStrips * NumLedStrips];
+    while (!stoppingToken.IsCancellationRequested)
+    {
+      await Task.Delay(10, stoppingToken);
+      _memService.Read(0, block);
+      for (var i = 0; i < NumLedStrips; i++)
+      {
+        var msg = new Message($"/led/{i}");
+        for (var j = 0; j < NumSegmentsPerLedStrips; j++)
+        {
+          msg.PushAtom((int)block[i * NumLedStrips + j]);
+        }
+
+        await msg.Send(_oscClient);
+      }
+    }
+  }
+
+  public override async Task StopAsync(CancellationToken cancellationToken)
+  {
+    _memService.Disconnect();
+    await base.StopAsync(cancellationToken);
+    await _oscClient.DisconnectAsync();
+  }
+}
---
+++ Hsp.LedStripController/Plugin.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Runtime.InteropServices;
+using Hsp.Osc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ReaSharp;
+
+namespace Hsp.LedStripController;
+
+public static class Plugin
+{
+  [UnmanagedCallersOnly(EntryPoint = "ReaperPluginEntry")]
+  public static int ReaperPluginEntry(nint hInstance, nint rec)
+  {
+    var host = Host.CreateDefaultBuilder()
+      .ConfigureLogging((context, lb) =>
+      {
+        lb.ClearProviders();
+        lb.SetMinimumLevel(LogLevel.Warning);
+#if DEBUG
+        lb.SetMinimumLevel(LogLevel.Debug);
+#endif
+        lb.AddConfiguration(context.Configuration.GetSection("Logging"));
+        lb.AddProvider(new ReaperConsoleLoggerProvider());
+      })
+      .ConfigureServices((context, sc) =>
+      {
+        sc.AddSingleton<ICommandRegistry, DefaultCommandRegistry>();
+        sc.AddSingleton<GmemService>();
+        sc.AddSingleton<IOscClient>(c => new OscUdpClient(IPAddress.Parse("127.0.0.1"), 9100, 9101));
+        sc.AddHostedService<GmemToOscDispatcher>();
+      })
+      .Build();
+
+    try
+    {
+      var state = PluginState.Initialize(ReaperPluginInfo.FromPointer(rec), host);
+      return 1;
+    }
+    catch (Exception ex)
+    {
+      return 0;
+    }
+  }
+}
